5ad8399e2e05ac8c7729ffadf8cf5090fa48b85b
vietviet08 <vietnq.23ceb@vku.udn.vn>
2024-10-06 23:53:10 +0700

    fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -38,5 +38,8 @@ export async function middleware(request: NextRequest){
 
     return NextResponse.next()
 
-
 }
+
+export const config = {
+    matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  };
