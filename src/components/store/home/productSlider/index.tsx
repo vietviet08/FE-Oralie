b4285a31fe86182194c9fd211eb2aee5a17be050
vietviet08 <vietnq.23ceb@vku.udn.vn>
2024-11-14 23:56:56 +0700

    custom(swiper): Custom navigation & button for product detail page

--- src/components/store/home/productSlider/index.tsx
+++ src/components/store/home/productSlider/index.tsx
@@ -1,31 +1,41 @@
 "use client";
 
 import "@/styles/custompaginationswiper.css";
-import { Autoplay, FreeMode, Pagination } from "swiper/modules";
+import { Autoplay, FreeMode, Pagination, Navigation } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import { ListResponse } from "@/model/ListData";
 import { Product } from "@/model/product/Product";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ProductCard from "../../product/product-card";
 import { getProductById } from "@/services/ProductService";
 import { Button } from "@/components/ui/button";
+import { Swiper as SwiperType } from "swiper/types";
 
 const ProductSlider: React.FC<ListResponse<Product>> = ({ data }) => {
-  useState<Product[]>(data);
   const [product, setProduct] = useState<Product>();
+
+  const prevRef = useRef<HTMLDivElement>(null);
+  const nextRef = useRef<HTMLDivElement>(null);
+  const swiperRef = useRef<SwiperType>();
 
   useEffect(() => {
     async function fetchData() {
       const response = await getProductById(12);
-      // const data = response.data;
       setProduct(response);
     }
 
     fetchData();
   }, []);
+
+  useEffect(() => {
+    if (swiperRef.current && swiperRef.current.navigation) {
+      swiperRef.current.navigation.init();
+      swiperRef.current.navigation.update();
+    }
+  }, [swiperRef.current]);
 
   return (
     <div>
@@ -49,16 +59,30 @@ const ProductSlider: React.FC<ListResponse<Product>> = ({ data }) => {
         </div>
       </div>
 
-      <section className="w-full h-[395px] rounded-lg  overflow-hidden">
+      <section className="w-full h-[395px] rounded-lg overflow-hidden">
         <div className="">
           <ul className="h-[395px] w-full">
             <Swiper
+              onSwiper={(swiper) => {
+                swiperRef.current = swiper;
+              }}
               slidesPerView={5}
               autoplay={true}
               spaceBetween={30}
               freeMode={true}
               pagination={{
                 clickable: true,
+              }}
+              style={
+                {
+                  "--swiper-navigation-color": "#DA2118",
+                  "--swiper-pagination-color": "#DA2118",
+                  "--swiper-navigation-size": "32px",
+                } as React.CSSProperties
+              }
+              navigation={{
+                prevEl: prevRef.current,
+                nextEl: nextRef.current,
               }}
               breakpoints={{
                 500: {
@@ -78,20 +102,19 @@ const ProductSlider: React.FC<ListResponse<Product>> = ({ data }) => {
                   spaceBetween: 10,
                 },
               }}
-              modules={[FreeMode, Pagination, Autoplay]}
+              modules={[FreeMode, Pagination, Autoplay, Navigation]}
               className="mySwiper h-full"
             >
               {product && (
                 <>
                   <SwiperSlide>
-                    <div className="w-full ">
+                    <div className="w-full">
                       <ProductCard product={product} />
                     </div>
                   </SwiperSlide>
                   <SwiperSlide>
                     <ProductCard product={product} />
                   </SwiperSlide>
-
                   <SwiperSlide>
                     <ProductCard product={product} />
                   </SwiperSlide>
@@ -106,6 +129,56 @@ const ProductSlider: React.FC<ListResponse<Product>> = ({ data }) => {
                   </SwiperSlide>
                 </>
               )}
+              <div
+                ref={prevRef}
+                style={{
+                  opacity: 0.5,
+                  position: "absolute",
+                  top: "50%",
+                  left: "10px",
+                  zIndex: 10,
+                  width: "40px",
+                  height: "40px",
+                  backgroundColor: "#DA2118",
+                  color: "#fff",
+                  borderRadius: "50%",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  cursor: "pointer",
+                  transition: "ease-in-out 0.2s",
+                  transform: "translateY(-50%)",
+                }}
+                onMouseEnter={(e) => (e.currentTarget.style.opacity = "1")}
+                onMouseLeave={(e) => (e.currentTarget.style.opacity = "0.5")}
+              >
+                &#10094;
+              </div>
+              <div
+                ref={nextRef}
+                style={{
+                  opacity: 0.5,
+                  position: "absolute",
+                  top: "50%",
+                  right: "10px",
+                  zIndex: 10,
+                  width: "40px",
+                  height: "40px",
+                  backgroundColor: "#DA2118",
+                  color: "#fff",
+                  borderRadius: "50%",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  cursor: "pointer",
+                  transition: "ease-in-out 0.2s",
+                  transform: "translateY(-50%)",
+                }}
+                onMouseEnter={(e) => (e.currentTarget.style.opacity = "1")}
+                onMouseLeave={(e) => (e.currentTarget.style.opacity = "0.5")}
+              >
+                &#10095;
+              </div>
             </Swiper>
           </ul>
         </div>
--- src/components/store/product/product-detail/index.tsx
+++ src/components/store/product/product-detail/index.tsx
@@ -2,7 +2,7 @@
 
 import { Badge } from "@/components/ui/badge";
 import { Product } from "@/model/product/Product";
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import {
   Autoplay,
   FreeMode,
@@ -19,7 +19,9 @@ import "swiper/css/free-mode";
 import "swiper/css/navigation";
 import "swiper/css/thumbs";
 import { Swiper as SwiperClass } from "swiper/types";
-
+import { Swiper as SwiperType } from "swiper/types";
+import { Button } from "@/components/ui/button";
+import { Icons } from "@/components/icons";
 type Props = {
   product: Product;
 };
@@ -27,6 +29,18 @@ type Props = {
 const ProductPageDetail = ({ product }: Props) => {
   const [thumbsSwiper, setThumbsSwiper] = useState<SwiperClass | null>(null);
   const [activeIndex, setActiveIndex] = useState(0);
+
+  const prevRef = useRef<HTMLDivElement>(null);
+  const nextRef = useRef<HTMLDivElement>(null);
+  const swiperRef = useRef<SwiperType>();
+
+  useEffect(() => {
+    if (swiperRef.current && swiperRef.current.navigation) {
+      swiperRef.current.navigation.init();
+      swiperRef.current.navigation.update();
+    }
+  }, [swiperRef.current]);
+
   return (
     <div className="sm:px-32 px-6 py-6 ">
       <div className="flex justify-between items-center w-5/6 py-4">
@@ -58,6 +72,9 @@ const ProductPageDetail = ({ product }: Props) => {
             <div className="w-full md:w-3/5 mr-0 md:mr-2 mt-0 p-4 rounded-lg shadow-md">
               <div className="relative w-full h-full rounded-lg  overflow-hidden">
                 <Swiper
+                  onSwiper={(swiper) => {
+                    swiperRef.current = swiper;
+                  }}
                   style={
                     {
                       "--swiper-navigation-color": "#DA2118",
@@ -66,7 +83,10 @@ const ProductPageDetail = ({ product }: Props) => {
                     } as React.CSSProperties
                   }
                   spaceBetween={10}
-                  navigation={true}
+                  navigation={{
+                    prevEl: prevRef.current,
+                    nextEl: nextRef.current,
+                  }}
                   thumbs={{ swiper: thumbsSwiper }}
                   // pagination={{ type: "bullets", clickable: true }}
                   autoplay={true}
@@ -80,11 +100,71 @@ const ProductPageDetail = ({ product }: Props) => {
                         <img
                           src={image.url}
                           alt={product.name}
-                          className="absolute left-0 top-0 w-full h-full object-cover"
+                          className="absolute left-0 top-0 w-full h-full object-contain"
                         />
                       </div>
                     </SwiperSlide>
                   ))}
+                  <div
+                    ref={prevRef}
+                    style={{
+                      opacity: 0.3,
+                      position: "absolute",
+                      top: "50%",
+                      left: "0",
+                      zIndex: 10,
+                      paddingLeft: "20px",
+                      width: "50px",
+                      height: "50px",
+                      backgroundColor: "#DA2118",
+                      color: "#fff",
+                      borderRadius: "50%",
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                      cursor: "pointer",
+                      transition: "ease-in-out 0.2s",
+                      transform: "translateY(-50%) translateX(-50%)",
+                    }}
+                    onMouseEnter={(e) =>
+                      (e.currentTarget.style.opacity = "0.7")
+                    }
+                    onMouseLeave={(e) =>
+                      (e.currentTarget.style.opacity = "0.3")
+                    }
+                  >
+                    &#10094;
+                  </div>
+                  <div
+                    ref={nextRef}
+                    style={{
+                      opacity: 0.3,
+                      position: "absolute",
+                      top: "50%",
+                      right: "0px",
+                      zIndex: 10,
+                      paddingLeft: "10px",
+                      width: "50px",
+                      height: "50px",
+                      backgroundColor: "#DA2118",
+                      color: "#fff",
+                      borderRadius: "50%",
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "start",
+                      cursor: "pointer",
+                      transition: "ease-in-out 0.2s",
+                      transform: "translateY(-50%) translateX(50%)",
+                    }}
+                    onMouseEnter={(e) =>
+                      (e.currentTarget.style.opacity = "0.8")
+                    }
+                    onMouseLeave={(e) =>
+                      (e.currentTarget.style.opacity = "0.3")
+                    }
+                  >
+                    &#10095;
+                  </div>
                 </Swiper>
                 <Swiper
                   onSwiper={setThumbsSwiper}
@@ -121,6 +201,38 @@ const ProductPageDetail = ({ product }: Props) => {
               </div>
             </div>
             <div className="w-full md:w-2/5 ml-0 md:ml-2 mt-0 p-4 rounded-lg shadow-md">
+              <div className="flex justify-around space-x-2 mb-4">
+                <div className="flex flex-col justify-center items-center w-1/3  rounded-xl border border-primaryred2 overflow-hidden">
+                  <div className="text-primaryred1 font-bold text-sm">
+                    16GB - 512GB
+                  </div>
+                  <div className="text-primaryred1 text-sm">$1111</div>
+                </div>
+                <div className="flex flex-col justify-center items-center w-1/3  rounded-xl border border-gray-300 overflow-hidden">
+                  <div className="text-primaryred1">16GB - 512GB</div>
+                  <div className="text-primaryred1">$1111</div>
+                </div>
+                <div className="flex flex-col justify-center items-center w-1/3  rounded-xl border border-gray-300 overflow-hidden">
+                  <div className="text-primaryred1">16GB - 512GB</div>
+                  <div className="text-primaryred1">$1111</div>
+                </div>
+              </div>
+
+              <div className="flex justify-between items-center space-x-3 my-4">
+                <div className="rounded-lg bg-primaryred w-5/6 h-14 flex flex-col justify-center items-center">
+                  <span className="text-sm text-white space-y-1 flex flex-col justify-center items-center">
+                    <span className="font-bold uppercase">Pay now</span>
+                    <span>Shiping or take direct in the store</span>
+                  </span>
+                </div>
+                <div className="h-14 flex items-center">
+                  <Button className="flex flex-col justify-center items-center rounded-lg border text-primaryred border-primaryred bg-white w-20 h-14 transition-all duration-300 ease-in-out hover:bg-primaryred hover:text-white">
+                    <Icons.shoppingCart className="w-10 h-10 text-xl" />
+                    <span className=" text-[12px]">Add to cart</span>
+                  </Button>
+                </div>
+              </div>
+
               <div className="flex flex-col space-y-2">
                 <div>
                   <h2 className="text-2xl font-bold text-primaryred">
@@ -129,7 +241,7 @@ const ProductPageDetail = ({ product }: Props) => {
                 </div>
                 <div>
                   <p className="text-lg font-semibold text-primaryred">
-                    {product.price} VND
+                    ${product.price}
                   </p>
                 </div>
                 <div>
