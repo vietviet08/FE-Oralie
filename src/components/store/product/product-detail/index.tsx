5472fee9eb1acf576c3ab1173f952012628c5bfe
vietviet08 <vietnq.23ceb@vku.udn.vn>
2024-12-20 13:54:59 +0700

    detach product detail page to children && responsive for product detail page

--- src/components/store/product/product-detail/index.tsx
+++ src/components/store/product/product-detail/index.tsx
@@ -20,19 +20,15 @@ import {Swiper as SwiperClass} from "swiper/types";
 import {Swiper as SwiperType} from "swiper/types";
 import {Button} from "@/components/ui/button";
 import {Icons} from "@/components/icons";
-import {
-    Table,
-    TableBody,
-    TableCell,
-    TableRow,
-} from "@/components/ui/table";
 import Image from "next/image";
 import {getTop10ProductRelatedCategory} from "@/services/ProductService";
-import Rates from "@/components/store/product/rates";
 import {addProductToCart} from "@/services/CartService";
 import {useSession} from "next-auth/react";
-import {useToast} from "@/hooks/use-toast";
 import Link from "next/link";
+import toast from 'react-hot-toast';
+import {FooterSologan} from "@/components/store/product/product-detail/footer-sologan";
+import {ProductSpecification} from "@/components/store/product/product-detail/product-specification";
+import {ProductPolicy} from "@/components/store/product/product-detail/product-policy";
 
 type Props = {
     product: Product;
@@ -42,7 +38,6 @@ const ProductPageDetail = ({product}: Props) => {
     const {data: session} = useSession();
     const token = session?.access_token as string;
 
-    const {toast} = useToast();
 
     const [selectedOptionId, setSelectedOptionId] = useState<number>(product.options[0].id!);
     const [quantity, setQuantity] = useState<number>(1);
@@ -79,10 +74,13 @@ const ProductPageDetail = ({product}: Props) => {
         const res = await addProductToCart(token, quantity, optionId, productId);
 
         if (res) {
-            toast({
-                variant: "success",
-                title: "Added to cart",
-                description: "Product has been added to cart successfully",
+            toast.success("Add to cart successfully", {
+                position: "bottom-right",
+                duration: 3000,
+            });
+        } else {
+            toast.error("Add to cart failed", {
+                position: "bottom-right",
                 duration: 3000,
             });
         }
@@ -91,14 +89,14 @@ const ProductPageDetail = ({product}: Props) => {
 
     return (
         <div className="sm:px-32 px-6 py-6 mt-14">
-            <div className="flex justify-between items-center w-full py-4">
-                <div className="w-3/5">
+            <div className="flex flex-col md:flex-row justify-between items-center gap-2 w-full py-4">
+                <div className="w-full md:w-4/5">
                     <h2 className="text-2xl font-bold text-black">{product.name}</h2>
                     <span className="text-sm my-2">
                         SKU: {product.sku} - Brand: {product.brand?.name}
                     </span>
                 </div>
-                <div className="">
+                <div className="w-full md:w-1/5 flex justify-start items-center md:justify-end">
                     {product.isAvailable ? (
                         <Badge
                             variant={"allower"}
@@ -119,8 +117,8 @@ const ProductPageDetail = ({product}: Props) => {
             <div className=" border-b-[1px] border-b-primaryred2"></div>
             <div className="pt-8">
                 <main>
-                    <div className="flex flex-col md:flex-row md:justify-center ">
-                        <div className="w-full md:w-3/5 mr-0 md:mr-2 mt-0 m-4 ml-0 ">
+                    <div className="flex flex-col lg:flex-row md:justify-center ">
+                        <div className="w-full lg:w-3/5 mr-0 md:mr-2 mt-0 m-4 ml-0 ">
                             <div className="relative w-full h-full ">
                                 <Swiper
                                     onSwiper={(swiper) => {
@@ -279,75 +277,11 @@ const ProductPageDetail = ({product}: Props) => {
                                         </SwiperSlide>
                                     ))}
                                 </Swiper>
-                                <div className="my-4 w-full md:w-3/5">
-                                    <h2 className="font-bold text-xl my-2">Product Policy</h2>
-                                    <div className="text-sm grid grid-cols-2 gap-3">
-                                        <div className="flex items-center gap-x-2 mb:gap-x-3">
-                                            <Image
-                                                src="https://oralie-bucket.s3.ap-southeast-1.amazonaws.com/Type_Hang_chinh_hang_0b0233a2c6.svg"
-                                                width={20}
-                                                height={20}
-                                                sizes="20px"
-                                                alt="return"
-                                            />
-                                            <span>Genuine product</span>
-                                        </div>
-                                        <div className="flex items-center gap-x-2 mb:gap-x-3">
-                                            <Image
-                                                src="https://oralie-bucket.s3.ap-southeast-1.amazonaws.com/Type_Bao_hanh_9415bfe460.svg"
-                                                width={20}
-                                                height={20}
-                                                sizes="20px"
-                                                alt="return"
-                                            />
-                                            <span>Warranty (months): 12</span>
-                                        </div>
-                                        <div className="flex items-center gap-x-2 mb:gap-x-3">
-                                            <Image
-                                                src="https://oralie-bucket.s3.ap-southeast-1.amazonaws.com/Type_Mien_phi_giao_hang_3339a0ce65.svg"
-                                                width={20}
-                                                height={20}
-                                                sizes="20px"
-                                                alt="return"
-                                            />
-                                            <span>Free delivery within 90 minutes</span>
-                                        </div>
-                                        <div className="flex items-center gap-x-2 mb:gap-x-3">
-                                            <Image
-                                                src="https://oralie-bucket.s3.ap-southeast-1.amazonaws.com/Type_Cai_dat_21382ecc84.svg"
-                                                width={20}
-                                                height={20}
-                                                sizes="20px"
-                                                alt="return"
-                                            />
-                                            <span>Free installation support</span>
-                                        </div>
-                                        <div className="flex items-center gap-x-2 mb:gap-x-3">
-                                            <Image
-                                                src="https://oralie-bucket.s3.ap-southeast-1.amazonaws.com/Type_Doi_tra_59d1881db4.svg"
-                                                width={20}
-                                                height={20}
-                                                sizes="20px"
-                                                alt="return"
-                                            />
-                                            <span>Return policy</span>
-                                        </div>
-                                        <div className="flex items-center gap-x-2 mb:gap-x-3">
-                                            <Image
-                                                src="https://oralie-bucket.s3.ap-southeast-1.amazonaws.com/Type_Tra_gop_0362e63008.svg"
-                                                width={20}
-                                                height={20}
-                                                sizes="20px"
-                                                alt="return"
-                                            />
-                                            <span>Installment policy</span>
-                                        </div>
-                                    </div>
-                                </div>
+                                <ProductPolicy/>
                             </div>
-
                         </div>
-                        <div className="w-full md:w-2/5 ml-0 md:ml-2 mt-0 m-4 mr-0 rounded-lg ">
+
+                        <div className="w-full lg:w-2/5 ml-0 md:ml-2 mt-0 m-4 mr-0 rounded-lg ">
                             <div className="grid grid-cols-3 gap-3 mb-4">
                                 {product.options.map((option) => (
                                     <Button
@@ -381,9 +315,9 @@ const ProductPageDetail = ({product}: Props) => {
                                 </div>
                             </div>
 
-                            <div className="my-4 space-x-3 flex justify-center items-center">
+                            <div className="my-4 flex flex-row lg:flex-col xl:flex-row justify-center items-center gap-2">
                                 <div
-                                    className="rounded-lg bg-primaryblue w-3/6 h-14 flex flex-col justify-center items-center">
+                                    className="rounded-lg bg-primaryblue w-3/6 md:w-full h-14 flex flex-col justify-center items-center m-0">
                                       <span
                                           className="text-sm text-white space-y-1 flex flex-col justify-center items-center">
                                         <span className="font-bold uppercase">INSTALLMENT 0%</span>
@@ -391,12 +325,12 @@ const ProductPageDetail = ({product}: Props) => {
                                       </span>
                                 </div>
                                 <div
-                                    className="rounded-lg bg-primaryblue w-3/6 h-14 flex flex-col justify-center items-center">
+                                    className="rounded-lg bg-primaryblue w-3/6 md:w-full h-14 flex flex-col justify-center items-center m-0">
                                       <span
                                           className="text-sm text-white space-y-1 flex flex-col justify-center items-center">
-                                        <span className="font-bold uppercase">
-                                          0% installment by card
-                                        </span>
+                                                <span className="font-bold uppercase">
+                                                  0% installment by card
+                                                </span>
                                         <span>(VISA, Mastercard, JCB)</span>
                                       </span>
                                 </div>
@@ -508,7 +442,6 @@ const ProductPageDetail = ({product}: Props) => {
                 </main>
             </div>
 
-
             <div className="py-6 mb-6 border-b-[1px] border-b-primaryred2"></div>
 
             {productsRelated && productsRelated.length > 0 &&
@@ -527,107 +460,9 @@ const ProductPageDetail = ({product}: Props) => {
                 </>
             }
 
-            <div
-                className="flex flex-col-reverse lg:flex-row space-x-0 lg:space-x-2 space-y-2 lg:space-y-0 justify-between">
-                <div className="lg:w-4/6 w-full p-4 pt-0 pl-0">
-                    <div className="mt-4 lg:mt-0 rounded-lg ">
-                        <h2 className="text-primaryred font-bold text-2xl mb-4">
-                            Detailed review {product.name}
-                        </h2>
-                        <div dangerouslySetInnerHTML={{__html: product.description}}/>
-                    </div>
+            <ProductSpecification product={product}/>
 
-                    <div className="py-2 my-4 border-b-[1px] border-b-primaryred2"></div>
-
-                    <Rates productId={product.id!} productName={product.name}/>
-                </div>
-                <div className="lg:w-2/6 w-full rounded-lg py-4">
-                    <h2 className=" font-bold text-lg">Specifications</h2>
-                    <Table>
-                        <TableBody>
-                            {product.specifications.map((spec, index) => (
-                                <TableRow
-                                    key={spec.id}
-                                    className={index % 2 === 0 ? "bg-white" : "bg-gray-100"}
-                                >
-                                    <TableCell className="w-2/6">{spec.name}</TableCell>
-                                    <TableCell className="w-4/6 text-left">{spec.value}</TableCell>
-                                </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </div>
-            </div>
-
-            <div className=" mb:border-t mb:border-neutral-gray-3 my-4 py-4">
-                <div className="">
-                    <div className="container">
-                        <div
-                            className=" flex-col md:flex-row justify-between items-center lg:grid-cols-4 grid grid-cols-2 gap-2">
-                            <div className="FeaturePolicy_item__Y5K8b">
-                                <div className="flex justify-center items-center">
-                                    <Image
-                                        sizes="65px"
-                                        width={65}
-                                        height={65}
-                                        src="https://oralie-bucket.s3.ap-southeast-1.amazonaws.com/policy1.svg"
-                                        alt={""}
-                                    />
-                                </div>
-                                <div className="text-[14px] text-center mt-4">
-                                    <p className="font-bold">Guaranteed Brand</p>
-                                    <p>Imported, Genuine Warranty</p>
-                                </div>
-                            </div>
-                            <div className="FeaturePolicy_item__Y5K8b">
-                                <div className="flex justify-center items-center">
-                                    <Image
-                                        sizes="65px"
-                                        width={65}
-                                        height={65}
-                                        src="https://oralie-bucket.s3.ap-southeast-1.amazonaws.com/policy2.svg"
-                                        alt={""}
-                                    />
-                                </div>
-                                <div className="text-[14px] text-center mt-4">
-                                    <p className="font-bold">Easy Return</p>
-                                    <p>According to Oralie&#39;s Return Policy</p>
-                                </div>
-                            </div>
-                            <div className="FeaturePolicy_item__Y5K8b">
-                                <div className="flex justify-center items-center">
-                                    <Image
-                                        sizes="65px"
-                                        width={65}
-                                        height={65}
-                                        src="https://oralie-bucket.s3.ap-southeast-1.amazonaws.com/policy3.svg"
-                                        alt={""}
-                                    />
-                                </div>
-                                <div className="text-[14px] text-center mt-4">
-                                    <p className="font-bold">Quality Products</p>
-                                    <p>Guaranteed Compatibility and Durability</p>
-                                </div>
-                            </div>
-                            <div className="FeaturePolicy_item__Y5K8b">
-                                <div className="flex justify-center items-center">
-                                    <Image
-                                        sizes="65px"
-                                        width={65}
-                                        height={65}
-                                        src="https://oralie-bucket.s3.ap-southeast-1.amazonaws.com/policy4.svg"
-                                        alt={""}
-                                    />
-                                </div>
-                                <div className="text-[14px] text-center mt-4">
-                                    <p className="font-bold">Delivery to Your Doorstep</p>
-                                    <p>In 63 Provinces</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <FooterSologan/>
         </div>
     );
 };
